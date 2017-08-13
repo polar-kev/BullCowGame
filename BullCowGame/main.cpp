8052f02292aa18bd2f06210ce4808d63a59e0e37
Kevin Semple <KSemp@falconpunch.local>
2017-08-12 23:48:38 -0400

    Better formatting

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -9,32 +9,25 @@
 #include <iostream>
 
 bool isogramCheck(std::string myword);
-int bullCheck(std::string goalWord, std::string guess);
-int cowCheck(std::string goalWord, std::string guess);
+int getBulls(std::string goalWord, std::string guess);
+int getCows(std::string goalWord, std::string guess);
 bool isValid(std::string goalWord,std::string myword);
-void init();
+void play();
 bool winCheck(std::string goalWord, std::string guess);
+void printIntroText();
+std::string getGuess();
 
 
 int main(int argc, const char * argv[]) {
     int choice;
-    std::string garbage;
     
     do{
-        std::cout   << "Welcome to Bulls and Cows.\n"
-                    << "Can you guess the isogram I've got in my head?\n"
-                    << "Please make a selection: \n"
-                    << "1.\t Play Bulls and Cows\n"
-                    << "2.\t Explain the Rules\n"
-                    << "3.\t Change the Difficulty\n"
-                    << "4.\t Quit\n";
+        printIntroText();
         std::cin >> choice;
-        
         switch(choice){
             case 1:
-                //initialize game
-                std::getline(std::cin,garbage);
-                init();
+                //play game
+                play();
                 break;
             case 2:
                 //Explain the Rules
@@ -49,42 +42,53 @@ int main(int argc, const char * argv[]) {
                 break;
         }
     }while(choice != 4);
-    
-   
-}
+}//end of main
 
 
 //initialize the game
-void init(){
+void play(){
     std::string guessWord;
     std::string goalWord = "clown";
-    int bulls;
-    int cows;
+    int bulls, cows;
     int attempts = 10;
     
+    //clear input buffer
+    std::string garbage;
+    std::getline(std::cin,garbage);
+    
     while(attempts > 0){
-        std::cout << "\nGuess a " << goalWord.length() << " letter word.\n";
-        std::cout << "You have " << attempts-- << " remaining attempts.\n";
-        std::getline(std::cin,guessWord);
+        std::cout   << "\nGuess a " << goalWord.length() << " letter word.\n"
+                    << "You have " << attempts-- << " remaining attempts.\n";
+        
+        guessWord = getGuess();
         std::cout << "You guessed: " << guessWord << std::endl;
+        
         if(isValid(goalWord, guessWord)){
-            bulls = bullCheck(goalWord, guessWord);
-            cows = cowCheck(goalWord, guessWord);
-            std::cout << "Bulls: " << bulls << std::endl;
-            std::cout << "Cows: " << cows << std::endl;
+            bulls = getBulls(goalWord, guessWord);
+            cows = getCows(goalWord, guessWord);
             
+            std::cout   << "Bulls: " << bulls << std::endl
+                        << "Cows: " << cows << std::endl;
+            
+            //only check win condition if valid input received
             if(winCheck(goalWord, guessWord)){
                 std::cout << "You win! The word was " << goalWord << "!" << std::endl << std::endl;
                 break;
             }
         }else{
-            std::cout << "Make sure you enter a valid word.\n";
+            std::cout << "Make sure you guess a valid isogram.\n";
         }
     }//end while
     if(attempts==0){
         std::cout << "You have 0 attempts left. YOU LOSE!\n\n";
     }
-}//end of init()
+}//end of play()
+
+std::string getGuess(){
+    std::string word;
+    std::getline(std::cin,word);
+    return word;
+}//end of getGuess
 
 //isogram is a word where each letter only appears once
 bool isogramCheck(std::string myword){
@@ -100,7 +104,7 @@ bool isogramCheck(std::string myword){
 }
 
 //Bull- letters in the guess word are in the same index position as the goal word
-int bullCheck(std::string myword, std::string guess){
+int getBulls(std::string myword, std::string guess){
     int count = 0;
     for(int i=0;i<myword.length();i++){
         if(myword[i] == guess[i]){
@@ -108,11 +112,11 @@ int bullCheck(std::string myword, std::string guess){
         }
     }
     return count;
-}//end of bullCheck
+}//end of getBulls
 
 
 //cow- a letter in the guess word can also be found in the goal word but the letters are not in the same index position
-int cowCheck(std::string myword, std::string guess){
+int getCows(std::string myword, std::string guess){
     int count = 0;
     for(int i=0;i<=5;i++){
         for(int j=0;j<=5;j++){
@@ -125,7 +129,7 @@ int cowCheck(std::string myword, std::string guess){
         }
     }
     return count;
-}//end of cowCheck
+}//end of getCows
 
 //The win condition for this game is that goalword matches the guessword
 bool winCheck(std::string goalWord, std::string guess){
@@ -141,3 +145,13 @@ bool isValid(std::string goalWord,std::string myword){
     }
     return true;
 }//end of isValid
+
+void printIntroText(){
+    std::cout   << "Welcome to Bulls and Cows.\n"
+                << "Can you guess the isogram I've got in my head?\n"
+                << "Please make a selection: \n"
+                << "1.\t Play Bulls and Cows\n"
+                << "2.\t Explain the Rules\n"
+                << "3.\t Change the Difficulty\n"
+                << "4.\t Quit\n";
+}
